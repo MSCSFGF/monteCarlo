550f09376c28d919679ebdb14ce69cd768672d9e
Juan Ortiz <MSCSFGF@users.noreply.github.com>
2015-04-15 12:48:24 -0400

    Update monteCarlo.py
    Some pep8 stuff

--- monteCarlo.py
+++ monteCarlo.py
@@ -1,68 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math 
+import math
 import time
-import random 
+import random
 
 
 def f(x):
-	return np.sin(x)
+    return np.sin(x)
 
 
 def g(x):
-	return np.cos(x)
+    return np.cos(x)
 
 
-def integrate(a,b,N):
-    se =0
-    so =0    
-    dab=(f(a)+f(b))
-    h=(b-a)/N
-    for i in range(1,N):
-        xi=a+h*i
-        if i%2==0:
-            se=se+2*f(xi)
+def integrate(a, b, N):
+    se = 0
+    so = 0
+    dab = (f(a) + f(b))
+    h = (b - a) / N
+    for i in range(1, N):
+        xi = a + h * i
+        if i % 2 == 0:
+            se += 2 * f(xi)
         else:
-            so=so+4*f(xi)
-    s=se+so+dab
-    return s *(h/3)
+            so = so + 4 * f(xi)
+    s = se + so + dab
+    return s * (h / 3)
 
 
-def trap(a,b,n):
-	x=a
-	h=(b-a)/n
-	total =0
-	for i in range(int(n)):
-		total= total+(f(x)+f(x+h))
-		x=x+h
-	return total*(h/2) 
+def trap(a, b, n):
+    x = a
+    h = (b - a) / n
+    total = 0
+    for i in range(int(n)):
+        total = total + (f(x) + f(x + h))
+        x = x + h
+    return total * (h / 2)
 
-st=time.time()
-x=[]
-y=[]
-startPoint=0
-endPoint=2
-for r in np.arange(startPoint,endPoint,.1):
-	x.append(r)
-	y.append(f(r))
-plt.plot(x,y,'-')
+st = time.time()
+x = []
+y = []
+startPoint = 0
+endPoint = 2
+for r in np.arange(startPoint, endPoint, .1):
+    x.append(r)
+    y.append(f(r))
+plt.plot(x, y, '-')
 
-xVal=[]
-yVal=[]
-total=0
-n=50000
-for r in range (n):
-	a=endPoint*random.random()
-	b=endPoint*random.random()
-	if b<f(a):
-		total+=1/n
-		xVal.append(a)
-		yVal.append(b)
+xVal = []
+yVal = []
+total = 0
+n = 50000
+for r in range(n):
+    a = endPoint * random.random()
+    b = endPoint * random.random()
+    if b < f(a):
+        total += 1 / n
+        xVal.append(a)
+        yVal.append(b)
 
-print("the monte carlo function result was ", total, " this result took " ,time.time()-st, " seconds")
-st=time.time()
-print("the trapazoid method result was  ",trap(startPoint,endPoint,2000) ," this result took " ,time.time()-st, " seconds")
-st=time.time()
-print("simpsons method produced the result " , integrate(startPoint,endPoint,80)," this result took " ,time.time()-st, " seconds")
-plt.plot(xVal,yVal,marker='o',linestyle='')
+print ("The monte carlo function result was ", total,
+       " this result took ", time.time() - st, " seconds")
+st = time.time()
+print ("The trapazoid method result was  ", trap(startPoint,
+       endPoint, 2000), " this result took ", time.time() - st, " seconds")
+st = time.time()
+print ("Simpsons method produced the result ", integrate(startPoint,
+       endPoint, 80), " this result took ", time.time() - st, " seconds")
+
+plt.plot(xVal, yVal, marker='o', linestyle='')
 plt.show()
