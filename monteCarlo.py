e1677d61acd87be22e5e97b5f7c4ecb06d6183b4
MSCSFGF <ortizub41@gmail.com>
2015-04-17 21:53:46 -0400

    Some pep8 stuff

--- monteCarlo.py
+++ monteCarlo.py
@@ -1,32 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math
+import math 
 import time
-import random
+import random 
+
+class MonteCarlo():
+    def __init__(self):
+        pass
+
+xVal = []
+yVal = []
 
 
 def f(x):
     return np.sin(x)
 
-
 def g(x):
     return np.cos(x)
 
-
-def integrate(a, b, N):
+def integrate(a,b,N):
     se = 0
-    so = 0
-    dab = (f(a) + f(b))
-    h = (b - a) / N
+    so = 0    
+    dab=(f(a) + f(b))
+    h=(b - a) / N
     for i in range(1, N):
         xi = a + h * i
         if i % 2 == 0:
-            se += 2 * f(xi)
+            se = se + 2 * f(xi)
         else:
             so = so + 4 * f(xi)
     s = se + so + dab
     return s * (h / 3)
-
 
 def trap(a, b, n):
     x = a
@@ -35,38 +39,42 @@ def trap(a, b, n):
     for i in range(int(n)):
         total = total + (f(x) + f(x + h))
         x = x + h
-    return total * (h / 2)
+    return total * (h / 2) 
 
+def monteCarlo(xRange, yRange, n):
+    global xVal, yVal
+    area = (xRange[0] + xRange[1]) * (xRange[0] + xRange[1])
+    total = 0
+    for r in range (n):
+        a = xRange[0] + xRange[1] * random.random()
+        b = yRange[0] + yRange[1] * random.random()
+        if b < f(a):
+            total += 1
+            xVal.append(a)
+            yVal.append(b)
+    return (total / n) * area
+
+xRange = [0,2]
+yRange = [0,2]
+startPoint = xRange[0]
+endPoint = xRange[1]
 st = time.time()
+
+print("the monte carlo function result was ", monteCarlo(xRange,
+      yRange, 500000), " this result took ", time.time() - st, " seconds")
+st = time.time()
+print("the trapazoid method result was  ", trap(startPoint, endPoint, 2000),
+      " this result took ", time.time() - st, " seconds")
+st = time.time()
+print("simpsons method produced the result ", integrate(startPoint, endPoint,
+      80), " this result took ", time.time() - st, " seconds")
 x = []
 y = []
-startPoint = 0
-endPoint = 2
 for r in np.arange(startPoint, endPoint, .1):
     x.append(r)
     y.append(f(r))
-plt.plot(x, y, '-')
-
-xVal = []
-yVal = []
-total = 0
-n = 50000
-for r in range(n):
-    a = endPoint * random.random()
-    b = endPoint * random.random()
-    if b < f(a):
-        total += 1 / n
-        xVal.append(a)
-        yVal.append(b)
-
-print ("The monte carlo function result was ", total,
-       " this result took ", time.time() - st, " seconds")
-st = time.time()
-print ("The trapazoid method result was  ", trap(startPoint,
-       endPoint, 2000), " this result took ", time.time() - st, " seconds")
-st = time.time()
-print ("Simpsons method produced the result ", integrate(startPoint,
-       endPoint, 80), " this result took ", time.time() - st, " seconds")
-
-plt.plot(xVal, yVal, marker='o', linestyle='')
+plt.plot(x, y, '-', color='blue')
+plt.plot(xVal,yVal,marker='o',linestyle='',color="green")
 plt.show()
+if __name__ == '__main__':
+    MonteCarlo()
